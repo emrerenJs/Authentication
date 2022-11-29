42c693f1720651cb3f2e7198d52cd4d5d90f134c
Emre EREN <emreeren@Emres-MacBook-Air.local>
2022-11-30 00:44:33 +0300

    fix: makeshift for email config

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,25 +2,28 @@ import { Request, Response } from "express";
 import { sendMail } from "../utils/mailer";
 import { CreateUserPayload } from "./user.schema";
 import { createUser } from "./user.service";
-import config from 'config';
+import config from "config";
 
-const email = config.get<string>('user');
+const email = config.get<string>("smtp");
 
-export const createUserHandler = async (req: Request<{}, {}, CreateUserPayload>, res: Response) => {
-    const user = req.body;
-    try {
-        const createdUser = await createUser(user);
-        await sendMail({
-            from: `${email}`,
-            to: user.email,
-            subject: `Please verify your account`,
-            text: `Verification code ${createdUser.verificationCode}. Id: ${createdUser._id}`
-        });
-        return res.send("User successfully created"); 
-    } catch (err: any) {
-        if(err.code === 11000) {
-            return res.status(409).send('Account already exists');
-        }
-        return res.status(400).send(err);
+export const createUserHandler = async (
+  req: Request<{}, {}, CreateUserPayload>,
+  res: Response
+) => {
+  const user = req.body;
+  try {
+    const createdUser = await createUser(user);
+    await sendMail({
+      from: `${email}`,
+      to: user.email,
+      subject: `Please verify your account`,
+      text: `Verification code ${createdUser.verificationCode}. Id: ${createdUser._id}`,
+    });
+    return res.send("User successfully created");
+  } catch (err: any) {
+    if (err.code === 11000) {
+      return res.status(409).send("Account already exists");
     }
-}
+    return res.status(400).send(err);
+  }
+};
